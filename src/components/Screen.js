39521dba0844485bc83a0bb97c4f167dfc79ac90
Marvin Lee <43216761+malee31@users.noreply.github.com>
2022-04-16 16:37:14 -0700

    More default styles for Screen.js

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -3,8 +3,14 @@ import { StyleSheet, View } from "react-native";
 
 const { screenStyles } = StyleSheet.create({
 	screenStyles: {
+		flexDirection: "column",
+		alignContent: "center",
+		flexGrow: 1,
+		width: "100%",
 		paddingHorizontal: "2%",
-		paddingVertical: "2%"
+		paddingVertical: "2%",
+		margin: 0,
+		padding: 0
 	}
 });
 
