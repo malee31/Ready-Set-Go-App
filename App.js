741b15c6f0e4bb0a5f8df6a8565b3fa807c39fa7
Marvin Lee <43216761+malee31@users.noreply.github.com>
2022-04-16 15:48:07 -0700

    Committing and removing SQLite

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import { StatusBar } from 'expo-status-bar';
 import { TabNavigator } from "./src/components/Tabs";
 import { colors } from "./constants.json";
+import { useEffect, useState } from "react";
+import { instantiate, lister } from "./src/utils/storage";
+import AppLoading from "expo-app-loading";
 
 const theme = {
 	...DefaultTheme, dark: false, roundness: 2, colors: {
@@ -14,6 +17,22 @@ const theme = {
 };
 
 export default function App() {
+	const [ready, setReady] = useState(false);
+	useEffect(() => {
+		instantiate()
+			.then(() => {
+				setReady(true);
+			});
+	}, []);
+
+	useEffect(() => {
+		if(ready) {
+			lister().then(console.log);
+		}
+	}, [ready]);
+
+	if(!ready) return (<AppLoading/>);
+
 	return (
 		<PaperProvider theme={theme}>
 			<StatusBar style="auto"/>
--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,1 +1,48 @@
-// Storage.js
+import { Platform } from "react-native";
+import * as SQLite from 'expo-sqlite';
+
+const DATABASE_NAME = "RSGDB.sqlite";
+const db = openDatabase();
+
+function openDatabase() {
+	if(Platform.OS === "web") {
+		return {
+			transaction: () => {
+				return {
+					executeSql: () => {},
+				};
+			}
+		};
+	}
+
+	return SQLite.openDatabase(DATABASE_NAME);
+}
+
+let instantiated = false;
+
+export function instantiate() {
+	return instantiated || (instantiated = new Promise((resolve, reject) => {
+		db.transaction(tx => {
+			tx.executeSql(
+				"CREATE TABLE IF NOT EXISTS entries (id INTEGER PRIMARY KEY NOT NULL, title TEXT, description TEXT, start INT, end INT);",
+				[],
+				resolve,
+				reject
+			);
+		});
+	}));
+}
+
+export function lister() {
+	console.log("LIST");
+	return new Promise((resolve, reject) => {
+		db.transaction(tx => {
+			tx.executeSql(
+				"SELECT name FROM sqlite_schema WHERE type='table' ORDER BY name;",
+				[],
+				resolve,
+				resolve
+			);
+		});
+	});
+}
