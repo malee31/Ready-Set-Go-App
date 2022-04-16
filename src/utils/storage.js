fd99a689381082295fc1f3ac843cd702a950eeb9
Marvin Lee <43216761+malee31@users.noreply.github.com>
2022-04-16 16:58:47 -0700

    Untested storage manager

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,15 +1,17 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { v4 as uuidv4 } from "uuid";
 
 /**
- * @type {Entry[]} Sector
+ * @type Sector - A chunk of data separated by months from storage
+ * @property {KEY} Where the chunk is stored. Used to resync the sector data
+ * @property {Entry[]} data Entries contained in the sector
  */
 
 /**
  * @type Entry - An event that can be timed in the app
  * @property {string} id Unique ID for the entry
  * @property {string|undefined} categoryId Unique ID for the category the event belongs to if exists
- * @property {string} title Title of entry. Size limit TBA
- * @property {string} title Description of entry. Size limit TBA
+ * @property {string} task Title of entry. Size limit TBA
  * @property {number} start Start time in epoch
  * @property {number} end End time in epoch
  */
@@ -17,6 +19,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 /**
  * @type Category - Category to group entries under
  * @property {string} id Unique ID for the category
+ * @property {string} name Name of category. Size limit TBA
  * @property {Entry[]} entries Entry items under this category
  */
 
@@ -53,8 +56,7 @@ export function addCategory(newCategory) {
 }
 
 export function getCategory(categoryId) {
-	const category = basicInfoObj.categories.find(category => category.id = categoryId);
-
+	return basicInfoObj.categories.find(category => category.id = categoryId);
 }
 
 export function makeIdentifier(year, month, id) {
@@ -67,23 +69,43 @@ export function makeIdentifier(year, month, id) {
 
 export async function readSector({ year, month }) {
 	const SECTOR_NAME = `${year}/${month}`;
+	const sector = {
+		KEY: SECTOR_NAME,
+		data: []
+	};
+
 	try {
 		const serializedSector = await AsyncStorage.getItem(SECTOR_NAME);
-		const sectorData = JSON.parse(serializedSector) || [];
-	} catch (err) {
+		const sectorData = JSON.parse(serializedSector);
+		if(sectorData) {
+			sector.data = sectorData;
+		}
+	} catch(err) {
 		// Cannot read sector
-		return [];
+	}
+	return sector;
+}
+
+export async function saveSector(sector) {
+	// TODO: Error handling
+	AsyncStorage.setItem(sector.KEY, JSON.stringify(sector.data));
+}
+
+export async function add(newEntry) {
+	newEntry.id = uuidv4();
+	const sector = await readSector(makeIdentifier(0, 0, newEntry.id));
+	sector.data.push(newEntry);
+	await saveSector(sector);
+	if(newEntry.categoryId) {
+		getCategory(newEntry.categoryId).push(newEntry.id);
+		await saveCategories();
 	}
 }
 
-export function add(identifier, newEntry) {
-
-}
-
 export function remove(identifier) {
-
+	// TODO: Complete
 }
 
 export function edit(identifier) {
-
+	// TODO: Complete
 }
