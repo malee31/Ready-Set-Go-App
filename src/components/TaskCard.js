31a39bc1a99ef274ded2f703589600414a05d94e
Marvin Lee <43216761+malee31@users.noreply.github.com>
2022-04-17 01:30:36 -0700

    Colored status bars

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -1,22 +1,45 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { Card, Paragraph, Title } from "react-native-paper";
 import moment from "moment";
 import React from "react";
+import { colors } from "../../constants.json";
 
-const {cardStyle} = StyleSheet.create({
-	cardStyle: {
+const taskCardStyles = StyleSheet.create({
+	overlayContainer: {
+		position: "relative",
 		width: "100%",
 		marginBottom: 10
+	},
+	subunderlay: {
+		width: "100%",
+		height: "100%",
+		position: "absolute",
+		backgroundColor: colors.lightgray,
+		borderWidth: 1,
+		borderColor: colors.darkgray,
+	},
+	underlay: {
+		backgroundColor: colors.light,
+		borderColor: "transparent"
+	},
+	card: {
+		width: "100%",
+		backgroundColor: "transparent"
 	}
-})
+});
 
-export default function TaskCard({ entry }) {
+export default function TaskCard({ entry, percent = 0 }) {
 	return (
-		<Card style={cardStyle}>
-			<Card.Content>
-				<Title>{entry.task}</Title>
-				<Paragraph>{moment(entry.start).format("L hh:mm A")} - {moment(entry.end).format("hh:mm A")}</Paragraph>
-			</Card.Content>
-		</Card>
+		<View style={taskCardStyles.overlayContainer}>
+			<View style={taskCardStyles.subunderlay}/>
+			<View style={StyleSheet.flatten([taskCardStyles.subunderlay, taskCardStyles.underlay, { width: `${percent}%` }])}/>
+			<Card style={taskCardStyles.card}>
+				<Card.Content>
+					<Title>{entry.task}</Title>
+					<Paragraph>{moment(entry.start).format("L hh:mm A")} - {moment(entry.end).format("hh:mm A")}</Paragraph>
+				</Card.Content>
+			</Card>
+		</View>
+
 	)
 }
--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,12 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { ScrollView, StyleSheet } from "react-native";
 import { colors } from "../../constants.json";
 import Screen from "../components/Screen";
 import DayControls from "../components/DayControls";
-import { useCurrentDate } from "../components/CurrentDateContext";
-import { momentSectorRead } from "../utils/storage";
 import TaskCard from "../components/TaskCard";
 import { useDayTasks } from "../components/DayTasksContext";
+import moment from "moment";
 
 const calendarStyles = StyleSheet.create({
 	screenOverrides: {
@@ -31,13 +30,17 @@ const calendarStyles = StyleSheet.create({
 });
 
 function CalendarList() {
-	const {entries} = useDayTasks();
+	const { entries } = useDayTasks();
 
 	return (
 		<ScrollView contentContainerStyle={calendarStyles.listContainer}>
-			{entries.map(entry => (
-				<TaskCard entry={entry} key={entry.id}/>
-			))}
+			{entries.map(entry => {
+				const diffSeconds = moment.duration(moment(entry.end).diff(moment(entry.start))).asSeconds();
+				const diffNowSeconds = moment.duration(moment(entry.end).diff(moment())).asSeconds();
+				const percent = diffNowSeconds < 0 ? 100 : diffNowSeconds <= diffSeconds ? (diffSeconds - diffNowSeconds) / diffSeconds * 100 : 0;
+				console.log(`Percent: ${diffNowSeconds}`);
+				return (<TaskCard entry={entry} percent={percent} key={entry.id}/>);
+			})}
 		</ScrollView>
 	);
 }
